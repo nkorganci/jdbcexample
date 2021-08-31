cf2a9b671ab071601f0532bccda8f274dbf51925
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-31 18:08:51 -0500

    jdbc

---
+++ src/main/java/techproed/Jdbcexample/App.java
@@ -0,0 +1,13 @@
+package techproed.Jdbcexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
